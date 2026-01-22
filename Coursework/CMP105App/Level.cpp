5b21260cbc6ce2c3b02b428d02dd071309bbafd4
MATTHEW RUSSELL <2501277@uad.ac.uk>
2026-01-22 14:49:42 +0000

    Added Basic Player Movement

--- Coursework/CMP105App/Level.cpp
+++ Coursework/CMP105App/Level.cpp
@@ -4,6 +4,9 @@ Level::Level(sf::RenderWindow& hwnd, Input& in) :
 	BaseLevel(hwnd, in)
 {
 	// initialise game objects
+	m_player.setRadius({ 10.f });
+	m_player.setFillColor(sf::Color::Green);
+	m_player.setPosition({300.f,300.f});
 
 }
 
@@ -15,18 +18,59 @@ void Level::handleInput(float dt)
 		std::cout << "left mouse pressed" << std::endl;
 	}
 
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::D)) 
+	{
+		m_buttonPressed = movement::RIGHT;
+	}
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::A)) 
+	{
+		m_buttonPressed = movement::LEFT;
+	}
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::W))
+	{
+		m_buttonPressed = movement::UP;
+	}
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::S))
+	{
+		m_buttonPressed = movement::DOWN;
+	}
+
+
 }
 
 // Update game objects
 void Level::update(float dt)
 {
+	switch (m_buttonPressed)
+	{
+	case movement::UP:
+		m_player.move({ 0.f, -m_speed * dt });
+		break;
+	case movement::DOWN:
+		m_player.move({ 0.f, m_speed * dt });
+		break;
+	case movement::RIGHT:
+		m_player.move({ m_speed * dt, 0.f });
+		break;
+	case movement::LEFT:
+		m_player.move({ -m_speed * dt, 0.f });
+	}
+
+	if (m_player.getPosition().x > m_window.getSize().x) 
+	{
+		m_player.setPosition({ 400, 300 });
+	}
+	if (m_player.getPosition().y > m_window.getSize().y)
+	{
+		m_player.setPosition({ 400, 300 });
+	}
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
-
+	m_window.draw(m_player);
 	endDraw();
 }
 
--- Coursework/CMP105App/Level.h
+++ Coursework/CMP105App/Level.h
@@ -7,13 +7,17 @@ public:
 	Level(sf::RenderWindow& window, Input& input);
 	~Level() override = default;
 
+	enum class movement { UP, DOWN, LEFT, RIGHT};
+	movement m_buttonPressed;
+
 	void handleInput(float dt) override;
 	void update(float dt);
 	void render();
 
 private:
 	// Default functions for rendering to the screen.
+	sf::CircleShape m_player;
 
 	// Default variables for level class.
-	
+	int m_speed = 200;
 };
